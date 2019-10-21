acac0f72cb72901686f3bb772f0c0adc9b34dcbb
Raj Saxena <raj@Rajs-MBP.local>
2019-10-21 08:25:50 +0200

    Initial commit

---
+++ src/main/kotlin/com/tbst/validation/ValidationApplication.kt
@@ -0,0 +1,11 @@
+package com.tbst.validation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidationApplication
+
+fun main(args: Array<String>) {
+	runApplication<ValidationApplication>(*args)
+}
